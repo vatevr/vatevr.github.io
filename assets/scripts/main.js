aa689edc677e0d84c8aee8231c60152982bd5717
Berrimor <Hamlet.lance@hotmail.com>
2016-01-21 23:37:51 +0100

    Assets

---
+++ assets/scripts/main.js
@@ -0,0 +1,8 @@
+(function($, window, document){
+  $(function(){
+  var map = L.mapbox.map('map', 'mapbox.outdoors', {
+    accessToken: 'pk.eyJ1IjoiaWNoYmlubGVlciIsImEiOiJjaWpvdHpnZmYwMDZ1dnhtM2Q3ZmNqMTBuIn0.RSn2KKBqyylsQfW3lWlJrg'
+  });
+
+  });
+})(jQuery, window, document);
